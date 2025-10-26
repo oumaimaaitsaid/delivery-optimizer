2c5b979ad6d636a97a6161e0e5d49490ed918f7b
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-26 19:30:05 +0100

    feat: ajout de l'entité Delivery avec relation vers Tour

--- src/main/java/com/delivery/optimizer/model/Delivery.java
+++ src/main/java/com/delivery/optimizer/model/Delivery.java
@@ -1,4 +1,30 @@
 package com.delivery.optimizer.model;
 
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Delivery {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private double latitude;
+    private double longitude;
+    private double weight;
+    private double volume;
+    private String timeWindow;
+
+    @Enumerated(EnumType.STRING)
+    private  DeliveryStatus status;
+
+    @ManyToOne
+    @JoinColumn(name ="tour_id")
+    private Tour tour;
 }
--- src/main/java/com/delivery/optimizer/model/Warehouse.java
+++
@@ -1,4 +0,0 @@
-package com.delivery.optimizer.model;
-
-public class Warehouse {
-}
