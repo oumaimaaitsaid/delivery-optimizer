43629623a03d6649f6182ec965450bef3fed1564
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-26 19:34:13 +0100

    feat(model): ajout de l'entité Vehicle

---
+++ src/main/java/com/delivery/optimizer/model/Vehicle.java
@@ -0,0 +1,21 @@
+package com.delivery.optimizer.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vehicle {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private VehicleType type;
+
+    private double maxWeight;
+    private double maxVolume;
+    private int maxDeliveries;
+}
