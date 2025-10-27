5d6f42aae1a92d8c572d1c702adee759c5c22998
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-27 16:05:42 +0100

    ajouter CalculateDistance in NearestNeighborOptimizer

--- src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
+++ src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
@@ -2,9 +2,11 @@ package com.delivery.optimizer.optimizer;
 
 import com.delivery.optimizer.model.Delivery;
 import com.delivery.optimizer.model.Warehouse;
+import com.delivery.optimizer.util.DistanceCalculator;
+
 import java.util.*;
 public class NearestNeighborOptimizer implements TourOptimizer{
-
+    private   DistanceCalculator  distanceCalculator;
     @Override
     public List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries) {
         List<Delivery> remaining = new ArrayList<>(deliveries);
@@ -19,7 +21,7 @@ public class NearestNeighborOptimizer implements TourOptimizer{
             double minDistance =Double.MAX_VALUE;
 
             for(Delivery d : remaining){
-                double distance =distance(currentLat,currentLon,d.getLatitude(),d.getLongitude());
+                double distance = distanceCalculator.calculateDistance(currentLat,currentLon,d.getLatitude(),d.getLongitude());
                 if(distance<minDistance){
 
                     minDistance=distance;
@@ -34,13 +36,5 @@ public class NearestNeighborOptimizer implements TourOptimizer{
 return result;
 
     }
-    private double distance(double lat1, double long1, double lat2,double long2) {
-        double R =6371;
-        double dLat = Math.toRadians(lat2-lat1);
-        double dLng = Math.toRadians(long2-long1);
-        double a = Math.sin(dLat/2) * Math.sin(dLat/2)
-                +Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-                * Math.sin(dLng/2) * Math.sin(dLng/2);
-        return R * 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-    }
+
 }
