585b2ea2a52f8f99ae3e8f54c0eec38a47a7229b
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-27 09:51:32 +0100

    Add TourOptimizer interface defining tour optimization contract

--- src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
+++ src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
@@ -1,4 +1,9 @@
 package com.delivery.optimizer.optimizer;
 
-public class NearestNeighborOptimizer {
+import com.delivery.optimizer.model.Delivery;
+import com.delivery.optimizer.model.Warehouse;
+import java.util.*;
+public class NearestNeighborOptimizer implements TourOptimizer{
+
+
 }
