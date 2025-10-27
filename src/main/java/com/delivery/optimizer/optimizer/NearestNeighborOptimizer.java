3fdf80b4ab27497cb807a1f1008262fcb1f89620
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-27 09:51:47 +0100

    Implement nearest neighbor algorithm to compute optimal delivery tour

--- src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
+++ src/main/java/com/delivery/optimizer/optimizer/NearestNeighborOptimizer.java
@@ -5,5 +5,26 @@ import com.delivery.optimizer.model.Warehouse;
 import java.util.*;
 public class NearestNeighborOptimizer implements TourOptimizer{
 
+    @Override
+    public List<Delivery> calculateOptimalTour(Warehouse warehouse, List<Delivery> deliveries) {
+        List<Delivery> remaining = new ArrayList<>(deliveries);
+        List<Delivery> result= new ArrayList<>();
+
+
+        double currentLat = warehouse.getLatitude();
+        double currentLon = warehouse.getLongitude();
+
+        while (!remaining.isEmpty()) {
+            Delivery nearest = remaining.stream()
+                    .min(Comparator.comparingDouble(d -> distance(currentLat, currentLon, d.getLatitude(), d.getLongitude())))
+                    .orElseThrow();
+            result.add(nearest);
+            currentLat = nearest.getLatitude();
+            currentLon = nearest.getLongitude();
+            remaining.remove(nearest);
+        }
+
+        return result;
+    }
 
 }
