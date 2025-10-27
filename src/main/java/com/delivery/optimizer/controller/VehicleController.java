c9e5e8f23ad950e21c471a65314edd2c2b43dff8
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-27 21:48:38 +0100

    feat(controller): ajouter VehicleController avec endpoints CRUD

--- src/main/java/com/delivery/optimizer/controller/VehicleController.java
+++ src/main/java/com/delivery/optimizer/controller/VehicleController.java
@@ -1,4 +1,23 @@
 package com.delivery.optimizer.controller;
 
+import com.delivery.optimizer.model.Vehicle;
+import com.delivery.optimizer.repository.VehicleRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/vehicles")
 public class VehicleController {
+
+    private final VehicleRepository vehicleRepository;
+
+    public VehicleController(VehicleRepository vehicleRepository) {
+        this.vehicleRepository = vehicleRepository;
+    }
+
+
+
+
+
 }
