2081dd13a4353978be2016267b6704ba00630e69
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-28 10:59:02 +0100

    refactor: modification du WarehouseController pour utiliser WarehouseDTO et WarehouseMapper

--- src/main/java/com/delivery/optimizer/controller/WarehouseController.java
+++ src/main/java/com/delivery/optimizer/controller/WarehouseController.java
@@ -1,10 +1,13 @@
 package com.delivery.optimizer.controller;
 
+import com.delivery.optimizer.dto.WarehouseDTO;
+import com.delivery.optimizer.mapper.WarehouseMapper;
 import com.delivery.optimizer.model.Warehouse;
 import com.delivery.optimizer.repository.WarehouseRepository;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/warehouses")
@@ -17,36 +20,43 @@ public class WarehouseController {
 
     //Get Recupérer tous les entrepôts
     @GetMapping
-    public List<Warehouse> getAll(){
-        return warehouseRepository.findAll();
+    public List<WarehouseDTO> getAll(){
+        return warehouseRepository.findAll()
+                .stream()
+                .map(WarehouseMapper::toDTO)
+                .collect(Collectors.toList());
     }
 
     //Get Recupérer les entreôts par ID
     @GetMapping("/{id}")
-    public Warehouse getById(@PathVariable Long id){
-        return warehouseRepository.findById(id).orElseThrow(()->new RuntimeException("Warehouse with id:"+id+" not found"));
+    public WarehouseDTO getById(@PathVariable Long id){
+       Warehouse warehouse= warehouseRepository.findById(id).orElseThrow(()->new RuntimeException("Warehouse with id:"+id+" not found"));
+       return WarehouseMapper.toDTO(warehouse);
     }
 
     //Post Ajouter un nouvel entrepôt
 
     @PostMapping
-    public Warehouse create(@RequestBody Warehouse warehouse){
-        return warehouseRepository.save(warehouse);
+    public WarehouseDTO create(@RequestBody WarehouseDTO dto){
+        Warehouse warehouse= WarehouseMapper.toEntity(dto);
+        Warehouse saved= warehouseRepository.save(warehouse);
+        return WarehouseMapper.toDTO(saved);
     }
 
     //PUT modifier un entrepôt
 
     @PutMapping("/{id}")
 
-    public Warehouse update(@PathVariable Long id,@RequestBody Warehouse updatedWarehouse){
+    public WarehouseDTO update(@PathVariable Long id,@RequestBody WarehouseDTO dto){
         Warehouse warehouse =warehouseRepository.findById(id)
                 .orElseThrow(()->new RuntimeException("Warehouse with id:"+id+" not found"));
-        warehouse.setName(updatedWarehouse.getName());
-        warehouse.setLatitude(updatedWarehouse.getLatitude());
-        warehouse.setLongitude(updatedWarehouse.getLongitude());
-        warehouse.setOpeningHours(updatedWarehouse.getOpeningHours());
+        warehouse.setName(dto.getName());
+        warehouse.setLatitude(dto.getLatitude());
+        warehouse.setLongitude(dto.getLongitude());
+        warehouse.setOpeningHours(dto.getOpeningHours());
 
-        return warehouseRepository.save(warehouse);
+         Warehouse updated =warehouseRepository.save(warehouse);
+        return  WarehouseMapper.toDTO(updated);
 
     }
     //Supprimer un entrepôt
