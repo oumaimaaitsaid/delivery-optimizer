c18fba26bf10ebf09d26d5c8881e715ee91faebf
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-28 09:43:33 +0100

    feat: ajout de la méthode toDTO pour convertir un Warehouse en WarehouseDTO

--- src/main/java/com/delivery/optimizer/mapper/WarehouseMapper.java
+++ src/main/java/com/delivery/optimizer/mapper/WarehouseMapper.java
@@ -5,6 +5,16 @@ import com.delivery.optimizer.model.Warehouse;
 
 public class WarehouseMapper {
 
+    public static WarehouseDTO toDTO(Warehouse warehouse){
+        WarehouseDTO dto =new WarehouseDTO();
+        dto.setId(warehouse.getId());
+        dto.setName(warehouse.getName());
+        dto.setLatitude(warehouse.getLatitude());
+        dto.setLongitude(warehouse.getLongitude());
+        dto.setOpeningHours(warehouse.getOpeningHours());
+        return dto;
 
+
+    }
 
 }
