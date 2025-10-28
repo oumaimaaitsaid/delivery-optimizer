814d5813035e36f3c654c97574a4ff16d7b78271
oumaimaaitsaid <62211685+oumaimaaitsaid@users.noreply.github.com>
2025-10-28 09:59:06 +0100

    feat: ajout du mapper DeliveryMapper
    - Création de la classe utilitaire DeliveryMapper
    - Ajout de la méthode toDTO() pour convertir un objet Delivery en DeliveryDTO
    - Ajout de la méthode toEntity() pour reconstruire un Delivery à partir d’un DTO
    - Gestion du lien entre Delivery et Tour via tourId
    - Facilite la séparation entre couche de persistance et couche présentation

--- src/main/java/com/delivery/optimizer/mapper/DeliveryMapper.java
+++ src/main/java/com/delivery/optimizer/mapper/DeliveryMapper.java
@@ -1,4 +1,35 @@
 package com.delivery.optimizer.mapper;
 
+import com.delivery.optimizer.dto.DeliveryDTO;
+import com.delivery.optimizer.model.Delivery;
+import com.delivery.optimizer.model.Tour;
+
 public class DeliveryMapper {
+
+    public static DeliveryDTO toDTO(Delivery delivery) {
+        DeliveryDTO dto = new DeliveryDTO();
+        dto.setId(delivery.getId());
+        dto.setLatitude(delivery.getLatitude());
+        dto.setLongitude(delivery.getLongitude());
+        dto.setWeight(delivery.getWeight());
+        dto.setVolume(delivery.getVolume());
+        dto.setTimeWindow(delivery.getTimeWindow());
+        dto.setStatus(delivery.getStatus());
+        dto.setTourId(delivery.getTour() != null ? delivery.getTour().getId() : null);
+        return dto;
+
+    }
+    public static Delivery toEntity(DeliveryDTO dto, Tour tour) {
+        Delivery delivery = new Delivery();
+        delivery.setId(dto.getId());
+        delivery.setLatitude(dto.getLatitude());
+        delivery.setLongitude(dto.getLongitude());
+        delivery.setWeight(dto.getWeight());
+        delivery.setVolume(dto.getVolume());
+        delivery.setTimeWindow(dto.getTimeWindow());
+        delivery.setStatus(dto.getStatus());
+        delivery.setTour(tour);
+        return delivery;
+    }
+
 }
